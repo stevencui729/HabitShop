0fc316903db81cb620f7afb7a294dc6ad24b4457
Steven <scui729@uchicago.edu>
2017-07-18 21:27:16 -0700

    first commit

---
+++ HabitShop/index.android.js
@@ -0,0 +1,103 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+// export default class HabitShop extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.android.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Double tap R on your keyboard to reload,{'\n'}
+//           Shake or press menu button for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+export default class HabitShop extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Blink style={styles.welcome}
+          text='Welcome to React Native!' />
+        <Blink style={styles.instructions}
+          text='To get started, edit index.android.js' />
+        <Blink style={styles.instructions}
+          text='Double tap R on your keyboard to reload,
+          Shake or press menu button for dev menu' />
+      </View>
+    );
+  }
+}
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
+  render() {
+    let display = this.state.showText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+// export default class BlinkApp extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Blink text='I love to blink' />
+//         <Blink text='Yes blinking is so great' />
+//         <Blink text='Why did they ever take this out of HTML' />
+//         <Blink text='Look at me look at me look at me' />
+//       </View>
+//     );
+//   }
+// }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('HabitShop', () => HabitShop);
